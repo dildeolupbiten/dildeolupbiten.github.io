5d89084cb0ac41a3c0f2792b372e20bd23c44f43
dildeolupbiten <tckutlu@gmail.com>
2026-06-15 14:36:20 +0200

    Update utils.js

--- apps/bayesian_probability/static/js/utils.js
+++ apps/bayesian_probability/static/js/utils.js
@@ -64,34 +64,3 @@ function *product(array, repeat) {
         }
     }
 }
-
-/*
-const p_prior = {true: 0.25, false: 0.75};
-
-const d1 = new CPTN({
-    "true":  {true: 0.9, false: 0.1},
-    "false": {true: 0.15, false: 0.85}
-}, [], "d1");
-
-const d2 = new CPTN({
-    "true": {true: 0.84, false: 0.16}, 
-    "false": {true: 0.22, false: 0.78}
-}, [], "d2");
-
-const d3 = new CPTN({
-    "true,true,true": {true: 0.98, false: 0.02},
-    "true,true,false": {true: 0.67, false: 0.33},
-    "true,false,true": {true: 0.53, false: 0.47},
-    "true,false,false": {true: 0.49, false: 0.51},
-    "false,true,true": {true: 0.65, false: 0.35},
-    "false,true,false": {true: 0.54, false: 0.46},
-    "false,false,true": {true: 0.41, false: 0.59},
-    "false,false,false": {true: 0.03, false: 0.97}
-}, [d1, d2], "d3");
-
-const result = [true, true, true];
-const dict = Object.fromEntries([...Array(cptn.length).keys()].map(i => [cptn[i].name, result[i]]));
-
-const prob = posteriorProbability(p_prior, dict);
-console.log(prob);
-*/
